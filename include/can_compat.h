773337f15658e9d9c0104f6fd5fdf23debb4c334
b <b@b3k.us>
2016-01-04 13:16:57 -0800

    Use socketcan data structures and defines

---
+++ include/can_compat.h
@@ -0,0 +1,38 @@
+#include <stdint.h>
+
+typedef uint8_t __u8;
+typedef uint32_t __u32;
+
+/* special address description flags for the CAN_ID */
+#define CAN_EFF_FLAG 0x80000000U /* EFF/SFF is set in the MSB */
+#define CAN_RTR_FLAG 0x40000000U /* remote transmission request */
+#define CAN_ERR_FLAG 0x20000000U /* error message frame */
+
+/* valid bits in CAN ID for frame formats */
+#define CAN_SFF_MASK 0x000007FFU /* standard frame format (SFF) */
+#define CAN_EFF_MASK 0x1FFFFFFFU /* extended frame format (EFF) */
+#define CAN_ERR_MASK 0x1FFFFFFFU /* omit EFF, RTR, ERR flags */
+
+typedef __u32 canid_t;
+
+#define CAN_SFF_ID_BITS11
+#define CAN_EFF_ID_BITS29
+
+typedef __u32 can_err_mask_t;
+
+#define CAN_MAX_DLC 8
+#define CAN_MAX_DLEN 8
+
+struct can_frame {
+    canid_t can_id;  /* 32 bit CAN_ID + EFF/RTR/ERR flags */
+    __u8    can_dlc; /* frame payload length in byte (0 .. CAN_MAX_DLEN) */
+    __u8    __pad;   /* padding */
+    __u8    __res0;  /* reserved / padding */
+    __u8    __res1;  /* reserved / padding */
+    __u8    data[CAN_MAX_DLEN] __attribute__((aligned(8)));
+};
+
+struct can_filter {
+    canid_t can_id;
+    canid_t can_mask;
+};
